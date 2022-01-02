098142ba2033adf6fee946c246f2b68375d03b3c
Joe Yang <joeking11829@gmail.com>
2022-01-03 07:59:24 +0800

    [DESC] 更新 Node-RED 套件： OM2M-One

--- docker/om2m_one/docker_run.sh
+++ docker/om2m_one/docker_run.sh
@@ -3,6 +3,9 @@
 docker run -ti --rm --name=om2m_one_iot_runtime \
            --privileged \
            -v $(pwd)/../../src:/root/src \
+           -v $(pwd)/../../src/data/in_database:/root/om2m/in-cse/database \
+           -v $(pwd)/../../src/data/mn_database:/root/om2m/mn-cse/database \
+           -v $(pwd)/../../src/node-red/package.json:/root/.node-red/package.json \
            -p 8080:8080 \
            -p 8282:8282 \
            -p 1880:1880 \
