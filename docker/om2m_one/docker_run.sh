e50e64445aa12ee6cc4c17e6e6195353b8184a4e
Joe Yang <joeking11829@gmail.com>
2022-01-03 21:07:24 +0800

    [DESC] Update        1. 指定 OM2M repo 使用 1.0.0 版本        2. 加入 Node-RED: device_ip flow (用於註冊 device_ip)

--- docker/om2m_one/docker_run.sh
+++ docker/om2m_one/docker_run.sh
@@ -5,7 +5,7 @@ docker run -ti --rm --name=om2m_one_iot_runtime \
            -v $(pwd)/../../src:/root/src \
            -v $(pwd)/../../src/data/in_database:/root/om2m/in-cse/database \
            -v $(pwd)/../../src/data/mn_database:/root/om2m/mn-cse/database \
-           -v $(pwd)/../../src/node-red/package.json:/root/.node-red/package.json \
+           -v $(pwd)/../../src/node-red/config/package.json:/root/.node-red/package.json \
            -p 8080:8080 \
            -p 8282:8282 \
            -p 1880:1880 \
